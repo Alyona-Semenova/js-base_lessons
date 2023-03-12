e898d647350c4a4c0426396ae50554887bd2d410
Alyona Semyonova <invisible2234@gmail.com>
2023-03-12 22:33:44 +0700

    Урок 5. Введение в DOM / 3 уровень/1 задание

--- lesson_5/3 уровень/1-tabs/app.js
+++ lesson_5/3 уровень/1-tabs/app.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 const texts = {
-    text1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    text2: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.',
-    text3: 'Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил.'
+    text1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
+    text2: "Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.",
+    text3: "Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил.",
 };
 
 /* 
@@ -12,32 +12,66 @@ const texts = {
     обработчик клика функцию clickHandler.
 */
 
+const text = document.querySelector(".text");
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", clickHandler);
+});
 
 /**
  * Обработчик клика по .nav-link
- * @param {MouseEvent} event 
+ * @param {MouseEvent} event
  */
 function clickHandler(event) {
+    changeActiveClass(event);
+    changeText(event);
     // здесь вызывайте changeText и changeActiveClass, и передавайте
     // им объект события.
-   
 }
 
 /**
  * Эта функция должна убирать .active у предыдущего .nav-link и ставить его
  * на тот, по которому кликнули.
- * @param {MouseEvent} event 
+ * @param {MouseEvent} event
  */
 function changeActiveClass(event) {
-    
+    navLinks.forEach((link) => {
+        link.classList.remove("active");
+    });
+
+    if (event.target.className != "active") {
+        event.target.classList.toggle("active");
+    }
 }
 
 /**
- * Эта фукнция должна читать текст (например через textContent) из 
+ * Эта фукнция должна читать текст (например через textContent) из
  * .nav-link по которому кликнули и в зависимости от этого в .text
  * ставить соответствующий текст из texts.
- * @param {MouseEvent} event 
+ * @param {MouseEvent} event
  */
 function changeText(event) {
-    
+    const currentLinkText = event.target.textContent; // текст текущей ссылки
+    //   const currentNumberLink = parseInt(currentLinkText.replace(/\D/g, "")); //номер ссылки
+
+    const textInput = document.querySelector(".text"); // поле с текстом
+
+    //   console.log(textInput)
+
+    for (let text in texts) {
+        if (findNumberOfText(currentLinkText) == findNumberOfText(text)) {
+            textInput.innerHTML = texts[text];
+        }
+    }
+}
+
+/**
+ * Метод принимает текст и ищет в нем цифры, если нашел - выводит их,
+ * если нет - возвращает false
+ * @param {String} text
+ */
+function findNumberOfText(text) {
+    return parseInt(text.replace(/\D/g, "")) || false;
 }
